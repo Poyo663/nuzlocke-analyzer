484703be4724ca28b60c7ecd4c08207b80694b39
Anna de Azevedo Maciel <annadeazevedo@annadeazevedo.annadeazevedo>
2024-06-10 20:03:26 -0300

    Added the home page plus the react rououter package

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl underline font-bold">Hello World</h1>
-    </>
+    <div className="canvas flex justify-around items-center bg-[url('/public/background.jpg')] bg-cover">
+      <div className="inline-block w-[60%]"></div>
+      <div className="w-[30%] h-[100%] flex-row content-around justify-center bg-yellow-400">
+        <p className="mt-[40%]">this is fucking dumb man</p>
+        <div className="w-[70%] px-7 py-5 rounded-[3rem] bg-red-500 transition-all duration-150 hover:translate-y-[-0.25rem]">
+          <a href="idk" className="text-sky-100">
+            Catch &apos;em all!
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        charizard: "url('/public/background.jpg')",
+      },
+    },
   },
   plugins: [],
 };
